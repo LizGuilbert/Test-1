c9f4315b1cb66f8bb469b1134db0add79c14ac3a
Ely <e.dehay@rubika-edu.com>
2022-02-14 12:21:05 +0100

    finalUpdate

--- Test Platformer/Assets/GlodSpawn.cs
+++ Test Platformer/Assets/GlodSpawn.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GlodSpawn : MonoBehaviour
 {
     public int gold;
+    public TextMeshProUGUI scoreText;
     void Start()
     {
         transform.position = new Vector3(Random.Range(-4.0f, 5.0f), transform.position.y,Random.Range(-6.5f,2.6f));
+        RefreshText();
     }
 
     // Update is called once per frame
     void OnCollisionEnter(Collision other)
     {
         transform.position = new Vector3(Random.Range(-4.0f, 5.0f), transform.position.y,Random.Range(-6.5f,2.6f));
-        gold += 1;
+        Addpoints(1);
+    }
+    
+    public void Addpoints(int points)
+    {
+        gold += points;
+        RefreshText();
+    }
+    
+    void RefreshText()
+    {
+        scoreText.text = "Score : " + gold;
     }
 }
